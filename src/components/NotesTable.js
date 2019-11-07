6c8c21c56d84f36dd37615bd1e443e4c3ea47617
dongare sachin <sachindongare029@gmail.com>
2019-11-07 10:55:02 +0530

    changes

--- src/components/NotesTable.js
+++ src/components/NotesTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './../styles/NotesTable.scss';
 
 class NotesTable extends Component {
@@ -7,27 +8,33 @@ class NotesTable extends Component {
   }
 
   render() {
-    console.log("props", this.props.notes);
+    let { notes } = this.props;
+    console.log("props", notes);
     return (
       <div className="notes">
         <table>
-          <th>
-            <td></td>
-            <td>Title</td>
-            <td>Content</td>
-          </th>
-          <tr>
-            <td></td>
-            <td>
-              hiii
-            </td>
-            <td>
-              hello
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th className="buttons-col"></th>
+              <th className="title-col">Title</th>
+              <th className="content-col">Content</th>
+              <th className="date-col">Updated Date</th>
+            </tr>
+            {notes.map((note, i)=>{
+              return (
+                <tr key={i}>
+                  <td></td>
+                  <td>{note.title}</td>
+                  <td>{note.content}</td>
+                  <td>{moment(note.date).format("YYYY-MM-DD hh:mm")}</td>
+                </tr>
+              );
+            })}
+            
+          </tbody>
         </table>
       </div>
-    )
+    );
   }
 }
 
