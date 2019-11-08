436e9489217ee1f2ac9be063e0352c12e98a2012
sachindongare029 <sachindongare029@gmail.com>
2019-11-08 14:31:04 +0530

    changes

--- src/components/NewNoteModal.js
+++ src/components/NewNoteModal.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
 import { Modal, Button } from "react-bootstrap";
-import './../styles/NewNoteModal.scss';
+import "./../styles/NewNoteModal.scss";
 
 class NewNoteModal extends Component {
-	// constructor(props) {
-	// 	super(props);
-	// }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      content: "",
+      tag: ""
+    };
+    this.handleSave = this.handleSave.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+  handleSave() {
+    let { title, tag, content } = this.state;
+    console.log("title, content, tag", title, content, tag);
+    // this.props.handleClose();
+  }
+
+  handleCancel() {
+    this.setState({
+      title: "",
+      content: "",
+      tag: ""
+    });
+    this.props.handleClose();
+  }
 
   render() {
+    let { title, tag, content } = this.state;
+    // console.log("title, content, tag", title, content, tag);
     return (
       <Modal
         show={this.props.show}
@@ -18,27 +41,46 @@ class NewNoteModal extends Component {
           <div className="modal_header">
             <i className="fa fa-file-text-o" aria-hidden="true"></i>
             <span className="modal_title">New Note</span>
-            <i className="fa fa-times" aria-hidden="true" onClick={() => this.props.handleClose()}></i>
+            <i
+              className="fa fa-times"
+              aria-hidden="true"
+              onClick={() => this.props.handleClose()}
+            ></i>
           </div>
           <div className="modal-input-container">
             <div>Title</div>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={e => {
+                this.setState({ title: e.target.value });
+              }}
+            />
             <div>Content</div>
-            <textarea rows="4" />
+            <textarea
+              rows="4"
+              onChange={e => {
+                this.setState({ content: e.target.value });
+              }}
+            />
             <div>Tags</div>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={e => {
+                this.setState({ tag: e.target.value });
+              }}
+            />
           </div>
-				<div className="button-group">
-					<Button variant="success" onClick={() => this.props.handleClose()}>
+          <div className="button-group">
+            <Button variant="success" onClick={() => this.handleSave()}>
               <i className="fa fa-floppy-o" aria-hidden="true"></i>
               Save
-          </Button>
-          <Button variant="danger" onClick={() =>this.props.handleClose()}>
-            <i className="fa fa-times" aria-hidden="true"></i>
-            Cancel
-          </Button>
-				</div>
-				</Modal.Body>
+            </Button>
+            <Button variant="danger" onClick={() => this.handleCancel()}>
+              <i className="fa fa-times" aria-hidden="true"></i>
+              Cancel
+            </Button>
+          </div>
+        </Modal.Body>
       </Modal>
     );
   }
